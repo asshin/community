686712866254119c58c8c56e4361ba9d3ffe22e0
wang <857541619@qq.com>
2023-07-07 10:40:12 +0800

    使用Aop打印日志信息

---
+++ src/main/java/com/cqupt/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,45 @@
+package com.cqupt.community.aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.cqupt.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        // 用户[1.2.3.4],在[xxx],访问了[com.nowcoder.community.service.xxx()].
+        /*
+        * RequestContextHolder.getRequestAttributes()：这是一个静态方法，用于获取与当前线程关联的当前请求属性。它返回一个RequestAttributes实例，它是用于访问请求属性的通用接口。
+(ServletRequestAttributes)：这是一个类型转换操作。它将getRequestAttributes()返回的RequestAttributes对象转换为ServletRequestAttributes。ServletRequestAttributes是Spring提供的RequestAttributes接口的特定实现，用于访问与Servlet相关的属性。
+attributes.getRequest()：这个方法调用从ServletRequestAttributes实例中获取底层的HttpServletRequest对象。getRequest()方法返回与当前线程关联的当前请求对象。
+通过执行这些代码行，您将可以访问HttpServletRequest对象，从而可以检索关于传入请求的信息，例如头信息、参数和请求URL等。*/
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].", ip, now, target));
+    }
+
+}
---
+++ src/main/java/com/cqupt/community/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.cqupt.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zsw
+ * @create 2023-07-07 10:37
+ */
+@Configuration
+public class RedisConfig {
+
+}
